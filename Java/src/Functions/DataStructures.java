701d486cf60f75f769441a6e52a87a130acfa2ca
bernardoadca18 <86448877+bernardoadca18@users.noreply.github.com>
2021-07-19 10:54:26 -0300

    Update DataStructures.java

--- Java/src/Functions/DataStructures.java
+++ Java/src/Functions/DataStructures.java
@@ -7,42 +7,7 @@ import java.util.Random;
 
 public class DataStructures 
 {
-    public static void main(String[] args) 
-    {
-        /*
-        List<List<Integer>> arr = new ArrayList<List<Integer>>();
-        
-        List<Integer> arrList1 = new ArrayList<Integer>();
-        List<Integer> arrList2 = new ArrayList<Integer>();
-        List<Integer> arrList3 = new ArrayList<Integer>();
-        List<Integer> arrList4 = new ArrayList<Integer>();
-        List<Integer> arrList5 = new ArrayList<Integer>();
-        List<Integer> arrList6 = new ArrayList<Integer>();
-
-        arr.add(arrList1);
-        arr.add(arrList2);
-        arr.add(arrList3);
-        arr.add(arrList4);
-        arr.add(arrList5);
-        arr.add(arrList6);
-        */
-
-        List<String> strings = new ArrayList<String>();
-        List<String> queries = new ArrayList<String>();
-
-        strings.add("aba");
-        strings.add("baba");
-        strings.add("aba");
-        strings.add("xzxb");
-
-        queries.add("aba");
-        queries.add("xzxb");
-        queries.add("ab");
-
-        System.out.println(matchingStrings(strings, queries));
-    }
-
-    public static List<Integer> matchingStrings(List<String> strings, List<String> queries)
+    public List<Integer> matchingStrings(List<String> strings, List<String> queries)
     {
         List<Integer> amount = new ArrayList<Integer>(queries.size());
 
@@ -67,7 +32,7 @@ public class DataStructures
         return amount;
     }
 
-    public static int hourglassSum(List<List<Integer>> arr)
+    public int hourglassSum(List<List<Integer>> arr)
     {
         List<Integer> sums = new ArrayList<Integer>();
 
@@ -82,7 +47,7 @@ public class DataStructures
         return Collections.max(sums);
     }
 
-    public static List<Integer> reverseArray(List<Integer> a) 
+    public List<Integer> reverseArray(List<Integer> a) 
     {
         List<Integer> reverseArray = new ArrayList<Integer>();
 
